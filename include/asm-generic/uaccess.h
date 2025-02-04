35342ceaab974cb70cb861b882696ebdbd8e83fe
dhelo11 <dhelo11@No-Mercy>
2025-02-04 10:55:01 +0000

    Revert "Upstream linux 4.9.341"
    This reverts commit 40c37645e2cb4e69f01347f37593b32dcb9db211.

--- include/asm-generic/uaccess.h
+++ include/asm-generic/uaccess.h
@@ -171,11 +171,8 @@ static inline __must_check long __copy_to_user(void __user *to,
 
 static inline int __put_user_fn(size_t size, void __user *ptr, void *x)
 {
-#ifdef CONFIG_ARCH_HAS_RAW_COPY_USER
-	return unlikely(raw_copy_to_user(ptr, x, size)) ? -EFAULT : 0;
-#else
-	return unlikely(__copy_to_user(ptr, x, size)) ? -EFAULT : 0;
-#endif
+	size = __copy_to_user(ptr, x, size);
+	return size ? -EFAULT : size;
 }
 
 #define __put_user_fn(sz, u, k)	__put_user_fn(sz, u, k)
@@ -236,11 +233,12 @@ extern int __put_user_bad(void) __attribute__((noreturn));
 #ifndef __get_user_fn
 static inline int __get_user_fn(size_t size, const void __user *ptr, void *x)
 {
-#ifdef CONFIG_ARCH_HAS_RAW_COPY_USER
-	return unlikely(raw_copy_from_user(x, ptr, size)) ? -EFAULT : 0;
-#else
-	return unlikely(__copy_from_user(x, ptr, size)) ? -EFAULT : 0;
-#endif
+	size_t n = __copy_from_user(x, ptr, size);
+	if (unlikely(n)) {
+		memset(x + (size - n), 0, n);
+		return -EFAULT;
+	}
+	return 0;
 }
 
 #define __get_user_fn(sz, u, k)	__get_user_fn(sz, u, k)
@@ -248,8 +246,6 @@ static inline int __get_user_fn(size_t size, const void __user *ptr, void *x)
 #endif
 
 extern int __get_user_bad(void) __attribute__((noreturn));
-
-#ifndef CONFIG_ARCH_HAS_RAW_COPY_USER
 
 #ifndef __copy_from_user_inatomic
 #define __copy_from_user_inatomic __copy_from_user
@@ -280,7 +276,6 @@ static inline long copy_to_user(void __user *to,
 	else
 		return n;
 }
-#endif
 
 /*
  * Copy a null terminated string from userspace.
