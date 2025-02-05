2754a0648f68002c9d7a0f940bf47516c308614b
dhelo11 <dhelo11@No-Mercy>
2025-02-05 12:50:19 +0000

    Revert "net: Don't reduce TTL/HL"
    This reverts commit 94a7e4f91f1f803b972d754879c1e6b324a29e0e.

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -456,7 +456,6 @@ int ip6_forward(struct sk_buff *skb)
 			return 0;
 	}
 
-#if 0
 	/*
 	 *	check and decrement ttl
 	 */
@@ -470,7 +469,6 @@ int ip6_forward(struct sk_buff *skb)
 		kfree_skb(skb);
 		return -ETIMEDOUT;
 	}
-#endif
 
 	/* XXX: idev->cnf.proxy_ndp? */
 	if (net->ipv6.devconf_all->proxy_ndp &&
@@ -562,9 +560,7 @@ int ip6_forward(struct sk_buff *skb)
 
 	/* Mangling hops number delayed to point after skb COW */
 
-#if 0
 	hdr->hop_limit--;
-#endif
 
 	return NF_HOOK(NFPROTO_IPV6, NF_INET_FORWARD,
 		       net, NULL, skb, skb->dev, dst->dev,
